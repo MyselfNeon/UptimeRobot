521798fad85a9259808d35d926fb6387c78fd8ef
NeonAnurag <justasneon@gmail.com>
2025-12-02 18:35:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,6 +16,14 @@ PORT = int(os.environ.get("PORT", "8080"))
 # List of URLs to monitor
 URLS = [
     "https://saverestrictions-bot.onrender.com/",
+    "https://website-monitor-ddy2.onrender.com/",
+    "https://telegraph-bot-q90q.onrender.com/",
+    "https://filestream-bot-njtx.onrender.com/",
+    "https://saverestrictions-bot.onrender.com/",
+    "https://filestore-bot-x4iy.onrender.com/",
+    "https://neonfilter-bot-ume4.onrender.com/",
+    "https://session-generator-bot-iq7n.onrender.com/",
+    "https://uptimerobot-oq3h.onrender.com",
     "https://telegraph-bot-q90q.onrender.com/",
 ]
 
