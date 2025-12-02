584a88142fd9ad78d9d8d227db9d5b601340c718
NeonAnurag <justasneon@gmail.com>
2025-12-02 15:53:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
 # Get these from https://my.telegram.org
-API_ID = int(os.environ.get("API_ID", "1234567"))
-API_HASH = os.environ.get("API_HASH", "your_api_hash_here")
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
 
 # Get this from @BotFather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token_here")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your User ID (Get from @userinfobot)
-ADMIN = int(os.environ.get("ADMIN", "123456789"))
+ADMIN = int(os.environ.get("ADMIN", "841851780"))
 
 # Server Port (Defaults to 8080 to satisfy cloud providers)
 PORT = int(os.environ.get("PORT", "8080"))
