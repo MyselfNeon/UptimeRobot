103637c9eac5eb13eca7507e0a8b1b3b91d98adc
NeonAnurag <justasneon@gmail.com>
2025-12-03 18:11:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,12 +10,14 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # Your User ID (Get from @userinfobot)
 ADMIN = int(os.environ.get("ADMIN", "841851780"))
 
+# Authorized Users (Separate multiple IDs with commas, e.g., "12345,67890")
+auth_users_raw = os.environ.get("AUTH_USERS", "")
+AUTH_USERS = {int(x) for x in auth_users_raw.split(",") if x.strip()}
+AUTH_USERS.add(ADMIN) # Ensure Admin is always authorized
+
 # Server Port
 PORT = int(os.environ.get("PORT", "8080"))
 
 # Database Configuration
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "UptimeBot")
-
-# Force Subscribe Channel ID (Default is the one you provided)
-FORCE_SUB = os.environ.get("FORCE_SUB", "-1002384933640")
