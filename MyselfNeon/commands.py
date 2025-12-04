178ad6a064594a9101e63b394f768250f7b11a73
NeonAnurag <justasneon@gmail.com>
2025-12-04 11:43:44 +0530

    Update commands.py

--- MyselfNeon/commands.py
+++ MyselfNeon/commands.py
@@ -1,4 +1,4 @@
-secondsrogram import Client, filters
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
 from info import AUTH_USERS
 from database import db
