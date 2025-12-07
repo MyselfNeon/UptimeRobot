4d22bc7abbf7a97dcc6bea71adeec1d96726776a
NeonAnurag <justasneon@gmail.com>
2025-12-07 13:59:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,13 +7,8 @@ API_HASH = os.environ.get("API_HASH", "")
 # Get this from @BotFather
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-# Your User ID (Get from @userinfobot)
-ADMIN = int(os.environ.get("ADMIN", "841851780"))
-
-# Authorized Users (Separate multiple IDs with commas, e.g., "12345,67890")
-auth_users_raw = os.environ.get("AUTH_USERS", "")
-AUTH_USERS = {int(x) for x in auth_users_raw.split(",") if x.strip()}
-AUTH_USERS.add(ADMIN) # Ensure Admin is always authorized
+# Admin ID (For startup logs only)
+ADMIN = int(os.environ.get("ADMIN", "0"))
 
 # Server Port
 PORT = int(os.environ.get("PORT", "8080"))
